41bfb476f3a61f0814cbe0c2342769614248d5cc
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-27 21:19:52 +0530

    using Services for http request

---
+++ src/app/posts.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs";
+
+@Injectable({providedIn: "root"})
+export class PostsService{
+    constructor(private http: HttpClient){}
+    createAndStorePost(title: string, content: string){
+        const postData: Post = {title: title, content: content} 
+        this.http
+      .post<{name: string}>(
+        'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+    }
+
+    fetchPosts(){
+        this.http
+      .get<{[key: string]: Post}>(
+        'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json'
+      ).pipe(map((responseData) => {
+        const postsArray: Post[] = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray
+      }))
+      .subscribe((posts) =>{
+        
+      });
+    }
+}
