516ed4c4650bd00d87e28c315790a6ecc9c60b42
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-27 20:50:41 +0530

    showing a loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true
     this.http
       .get<{[key: string]: Post}>(
         'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json'
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
         return postsArray
       }))
       .subscribe((posts) =>{
+        this.isFetching = false
         this.loadedPosts = posts
       });
   }
