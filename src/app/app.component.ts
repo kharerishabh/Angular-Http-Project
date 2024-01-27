24c7dae4754cb10f06b31b6e59b3a8b91f377a0a
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-27 17:08:37 +0530

    tranforming data using RXJS opreator pipe and map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -40,7 +41,15 @@ export class AppComponent implements OnInit {
     this.http
       .get(
         'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json'
-      )
+      ).pipe(map((responseData) => {
+        const postsArray = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray
+      }))
       .subscribe((posts) => console.log(posts));
   }
 }
