13eb6f7b08ea3a852740df49e472e222cdef364b
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-28 16:47:16 +0530

    adding multiple query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,50 +1,62 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Subject, catchError, map, throwError } from "rxjs";
-import { Post } from "./post.model";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Subject, catchError, map, throwError } from 'rxjs';
+import { Post } from './post.model';
 
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  error = new Subject<string>();
 
+  constructor(private http: HttpClient) {}
 
-@Injectable({providedIn: "root"})
-export class PostsService{
-  error = new Subject<string>()
-
-    constructor(private http: HttpClient){}
-    
-    createAndStorePost(title: string, content: string){
-        const postData: Post = {title: title, content: content} 
-        this.http
-      .post<{name: string}>(
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
         'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json',
         postData
       )
-      .subscribe((responseData) => {
-        console.log(responseData);
-      }, error => {
-        this.error.next(error.message)
-      });
-    }
-
-    fetchPosts(){
-       return this.http
-      .get<{[key: string]: Post}>(
-        'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json', {
-          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
         }
-      ).pipe(map((responseData) => {
-        const postsArray: Post[] = [];
-        for(const key in responseData){
-          if(responseData.hasOwnProperty(key)){
-            postsArray.push({...responseData[key], id: key})
+      );
+  }
+
+  fetchPosts() {
+    let searchParams = new HttpParams()
+    searchParams = searchParams.append('print', 'pretty')
+    searchParams = searchParams.append('custom', 'key')
+    return this.http
+      .get<{ [key: string]: Post }>(
+        'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams
+        }
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
           }
-        }
-        return postsArray
-      }), catchError (errorRes => {
-        return throwError(errorRes)
-      }))
-    }
+          return postsArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
+        })
+      );
+  }
 
-    deletePosts(){
-       return this.http.delete('https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json')
-    }
+  deletePosts() {
+    return this.http.delete(
+      'https://angular-project-563c1-default-rtdb.firebaseio.com/posts.json'
+    );
+  }
 }
