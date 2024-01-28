17f4970466c09bcdef7289bf65861bbfc187046b
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-28 19:28:36 +0530

    using multiple interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,15 +3,7 @@ import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request its on its way')
-        console.log(req.url)
         const modifyRequest = req.clone({headers: req.headers.append('Auth', 'xyz')})
-        return next.handle(modifyRequest).pipe(tap(event => {
-            console.log(event)
-            if(event.type === HttpEventType.Response){
-                console.log('Response arrived, body data: ')
-                console.log(event.body)
-            }
-        }))
+        return next.handle(modifyRequest)
     }
 }
