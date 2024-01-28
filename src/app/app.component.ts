b6e23f1de3367a906b8ff9b5ff93eec57af78ebd
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-28 15:47:15 +0530

    error handling and Ux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class AppComponent implements OnInit, OnDestroy{
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error => {
+      this.isFetching = false
       this.error = error.message
     });
   }
@@ -41,6 +42,7 @@ export class AppComponent implements OnInit, OnDestroy{
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error => {
+      this.isFetching = false
       this.error = error.message
     });
   }
@@ -50,6 +52,10 @@ export class AppComponent implements OnInit, OnDestroy{
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = []
     })
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 
   ngOnDestroy(): void {
